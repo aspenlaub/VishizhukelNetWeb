4cadcdff73473e4e604afc5bd169b32ef6bb4b4b
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2022-05-27 22:43:43 +0200

    Add remaining test cases as one

--- src/Integration/Test/TestCasesTest.cs
+++ src/Integration/Test/TestCasesTest.cs
@@ -1,7 +1,6 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Tash;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNetWeb.Test.WebView2Application.Entities;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNetWeb.Test.WebView2Application.GUI;
-using Aspenlaub.Net.GitHub.CSharp.VishizhukelNetWeb.Test.WebView2Application.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VishizhukelNetWebTestResources = Aspenlaub.Net.GitHub.CSharp.VishizhukelNetWeb.Test.Properties.Resources;
 
@@ -11,30 +10,47 @@ namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNetWeb.Integration.Test;
 public class TestCasesTest : IntegrationTestBase {
     [TestMethod]
     public async Task CanRunAlwaysSucceedsTestCase() {
-        await RunTestCaseAsync(VishizhukelNetWebTestResources.AlwaysSucceeds, "");
+        await RunTestCaseAsync(VishizhukelNetWebTestResources.AlwaysSucceeds);
+    }
+
+    [TestMethod]
+    public async Task CanRunCanFindTestCases() {
+        var testCaseNames = new List<string> {
+            VishizhukelNetWebTestResources.CanFindAnchor, VishizhukelNetWebTestResources.CanFindBody,
+            VishizhukelNetWebTestResources.CanFindLamasMainByClass, VishizhukelNetWebTestResources.CannotFindAnchorWhichIsNotDivLike
+        };
+
+        await RunTestCasesAsync(testCaseNames);
     }
 
     [TestMethod]
     public async Task CanRunAlwaysFailsTestCase() {
-        await RunTestCaseAsync(VishizhukelNetWebTestResources.AlwaysFails, VishizhukelNetWebTestResources.AlwaysFails);
-    }
-
-    public async Task RunTestCaseAsync(string testCaseName, string errorMessage) {
         using var sut = await CreateWindowUnderTestAsync(nameof(VishizhukelNetWebView2Window));
         var process = await sut.FindIdleProcessAsync();
+        var testCaseName = VishizhukelNetWebTestResources.AlwaysFails;
         var tasks = new List<ControllableProcessTask> {
             sut.CreateSetValueTask(process, nameof(ApplicationModel.SelectedTestCase), testCaseName),
         };
-        if (string.IsNullOrEmpty(errorMessage)) {
+        await sut.RemotelyProcessTaskListAsync(process, tasks);
+        await sut.RemotelyPressButtonAsync(process, nameof(ApplicationModel.RunTestCase), false);
+        tasks.Clear();
+        tasks.Add(sut.CreateVerifyValueTask(process, nameof(ApplicationModel.Status), testCaseName));
+        await sut.RemotelyProcessTaskListAsync(process, tasks);
+    }
+
+    public async Task RunTestCaseAsync(string testCaseName) {
+        await RunTestCasesAsync(new List<string> { testCaseName });
+    }
+
+    public async Task RunTestCasesAsync(List<string> testCaseNames) {
+        using var sut = await CreateWindowUnderTestAsync(nameof(VishizhukelNetWebView2Window));
+        var process = await sut.FindIdleProcessAsync();
+        var tasks = new List<ControllableProcessTask>();
+        foreach(var testCaseName in testCaseNames) {
+            tasks.Add(sut.CreateSetValueTask(process, nameof(ApplicationModel.SelectedTestCase), testCaseName));
             tasks.Add(sut.CreatePressButtonTask(process, nameof(ApplicationModel.RunTestCase)));
         }
-        await sut.RemotelyProcessTaskListAsync(process, tasks);
-        if (!string.IsNullOrEmpty(errorMessage)) {
-            await sut.RemotelyPressButtonAsync(process, nameof(ApplicationModel.RunTestCase), false);
 
-            tasks.Clear();
-            tasks.Add(sut.CreateVerifyValueTask(process, nameof(ApplicationModel.Status), errorMessage));
-            await sut.RemotelyProcessTaskListAsync(process, tasks);
-        }
+        await sut.RemotelyProcessTaskListAsync(process, tasks);
     }
 }
